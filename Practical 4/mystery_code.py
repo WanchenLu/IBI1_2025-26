628d4cfa08f0f08f1c3f263718ac41d241d63cdd
Lu Wanchen <wanchen.25@intl.zju.edu.cn>
2026-03-10 15:51:33 +0800

    Add pseudocode comment & answer

--- Practical 4/mystery_code.py
+++ Practical 4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:
+# Answer: the code generates 11 intergers between [1,10) randomly and outputs their summary
 
 # Import libraries
 # randint allows drawing a random number,
@@ -10,12 +10,15 @@ from random import randint
 # e.g. ceil(4.2)=5
 from math import ceil
 
+# Create two variables
 total_rand = 0
 progress=0
+# After a loop, progress increases by 1, total_rand increases by [1,10) randomly, and the loop carries on until progress becomes 10
 while progress<=10:
 	progress+=1
 	n = randint(1,10)
 	total_rand+=n
 
+# After 11 loops, it displays total_rand at the end
 print(total_rand)
 
