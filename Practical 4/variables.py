5972da69ace10c0155b182550ff454b995bbbdd7
Lu Wanchen <wanchen.25@intl.zju.edu.cn>
2026-03-10 15:22:52 +0800

    Add variables

---
+++ Practical 4/variables.py
@@ -0,0 +1,13 @@
+a = 5.08
+b = 5.33
+c = 5.55
+d = b-a
+e = c-b
+if e > d:
+    print ("Population growth is accelerating in Scotland")
+else :
+    print ("Population growth is decelerating in Scotland")
+X = True
+Y = False
+W = X or Y
+print (W)
