0096e87ad596325533dda721d8d0031b846517c6
Lu Wanchen <wanchen.25@intl.zju.edu.cn>
2026-04-01 15:44:27 +0800

    change the format of the output

--- Practical 7/stop_codons.py
+++ Practical 7/stop_codons.py
@@ -1,12 +1,32 @@
 import re
 input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
+sequence = ''
+gene_name = ''
 for line in input:
     if line.startswith('>'):
-        gene_name = re.findall (r'gene:(.+?)\s',line)
+        if gene_name != '':
+            stop_codons = []
+            for  match in re.finditer (r'ATG(?:...)+?(TAA|TAG|TGA)',sequence):
+                stop_codons.append(match.group(1))
+            if stop_codons:
+                content = f">{gene_name};{stop_codons}\n"
+                out_file = open('stop_genes.fa', 'a')
+                out_file.write(content)
+                out_file.write(f"{sequence}\n")
+                out_file.close()
+            sequence = ''
+            gene_name = re.findall (r'gene:(.+?)\s',line)
+        else :
+            gene_name = re.findall (r'gene:(.+?)\s',line)
     else:
-        stop_codons = re.findall (r'ATG(?:...)+?(TAA|TAG|TGA)',line)
-        if gene_name and stop_codons:
-            content = f"{gene_name}{stop_codons}\n"
-            out_file = open('stop_genes.fa', 'a')
-            out_file.write(content)
-            out_file.close()
+        line = line.strip()
+        sequence += line
+stop_codons = []
+for match in re.finditer (r'ATG(?:...)+?(TAA|TAG|TGA)',sequence):
+    stop_codons.append(match.group(1))
+if stop_codons:
+    content = f">{gene_name};{stop_codons}\n"
+    out_file = open('stop_genes.fa', 'a')
+    out_file.write(content)
+    out_file.write(f"{sequence}\n")
+    out_file.close()
