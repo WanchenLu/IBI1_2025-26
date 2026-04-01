31f8a2516a6b207210a2ce1061b8bb500b973f26
Lu Wanchen <wanchen.25@intl.zju.edu.cn>
2026-04-01 22:46:02 +0800

    complete task 1&2

---
+++ Practical 7/count_codons.py
@@ -0,0 +1,24 @@
+stop = input ("please input one of three possible stop codons (TAA/TAG/TGA): ")
+import re
+from collections import Counter
+input = open ('stop_genes.fa','r')
+codons = []
+upstream = []
+for line in input:
+    if line.startswith('>'):
+        if re.search(stop,line):
+            state = 'yes'
+        else:
+            state = 'no'
+    else:
+        if state == 'yes':
+            upstream.extend(re.findall(r'(ATG(?:.{3})+?)'+stop,line))
+        else:
+            continue
+longest = max(upstream,key=len)
+for i in range (0,len(longest),3):
+    codon = longest[i:i+3]
+    codons.append(codon)
+count = Counter(codons)
+for codon,count in count.most_common():
+    print(f"{codon}:{count}")
