b6d8467f790c558ea239a37db4aadba6a1e1affb
Lu Wanchen <wanchen.25@intl.zju.edu.cn>
2026-04-02 17:29:48 +0800

    complete task 3

--- Practical 7/count_codons.py
+++ Practical 7/count_codons.py
@@ -20,5 +20,15 @@ for i in range (0,len(longest),3):
     codon = longest[i:i+3]
     codons.append(codon)
 count = Counter(codons)
-for codon,count in count.most_common():
-    print(f"{codon}:{count}")
+for codon,cnt in count.most_common():
+    print(f"{codon}:{cnt}")
+sort = count.most_common()
+codon_list = [item[0] for item in sort]
+count_list = [item[1] for item in sort]
+import matplotlib.pyplot as plt
+sizes = count_list
+plt.pie(sizes,labels=codon_list,autopct='%1.1f%%',startangle=90)
+plt.axis('equal')
+plt.title("the distribution	of all in-frame codons")
+plt.savefig('codon_usage.png')
+plt.close()
