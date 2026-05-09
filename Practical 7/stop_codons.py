d49b1dd3d9010f169a6233b7b32c167a4d65d610
Lu Wanchen <wanchen.25@intl.zju.edu.cn>
2026-05-09 12:16:08 +0800

    delete atg search, extract stop codons as list, avoid stop codons duplicate, open outfile throughout

--- Practical 7/stop_codons.py
+++ Practical 7/stop_codons.py
@@ -1,32 +1,31 @@
 import re
-input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
-sequence = ''
-gene_name = ''
-for line in input:
-    if line.startswith('>'):
-        if gene_name != '':
-            stop_codons = []
-            for  match in re.finditer (r'ATG(?:...)+?(TAA|TAG|TGA)',sequence):
-                stop_codons.append(match.group(1))
-            if stop_codons:
-                content = f">{gene_name};{stop_codons}\n"
-                out_file = open('stop_genes.fa', 'a')
-                out_file.write(content)
-                out_file.write(f"{sequence}\n")
-                out_file.close()
-            sequence = ''
-            gene_name = re.findall (r'gene:(.+?)\s',line)
-        else :
-            gene_name = re.findall (r'gene:(.+?)\s',line)
-    else:
-        line = line.strip()
-        sequence += line
-stop_codons = []
-for match in re.finditer (r'ATG(?:...)+?(TAA|TAG|TGA)',sequence):
-    stop_codons.append(match.group(1))
-if stop_codons:
-    content = f">{gene_name};{stop_codons}\n"
-    out_file = open('stop_genes.fa', 'a')
-    out_file.write(content)
-    out_file.write(f"{sequence}\n")
-    out_file.close()
+with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') as input,\
+    open ('stop_genes.fa', 'w') as out_file:
+    sequence = ''
+    gene_name = ''
+    for line in input:
+        if line.startswith('>'):
+            if gene_name != '':
+                stop_codons = []
+                for codon in ['TAA','TGA','TAG']:
+                    if codon in sequence:
+                        stop_codons.append(codon)
+                if stop_codons:
+                    content = f">{gene_name};{",".join(stop_codons)}\n"
+                    out_file.write(content)
+                    out_file.write(f"{sequence}\n")
+                sequence = ''
+                gene_name = re.findall (r'gene:(.+?)\s',line)
+            else :
+                gene_name = re.findall (r'gene:(.+?)\s',line)
+        else:
+            line = line.strip()
+            sequence += line
+    stop_codons = []
+    for codon in ['TAA','TGA','TAG']:
+        if codon in sequence:
+            stop_codons.append(codon)
+    if stop_codons:
+        content = f">{gene_name};{",".join(stop_codons)}\n"
+        out_file.write(content)
+        out_file.write(f"{sequence}\n")
